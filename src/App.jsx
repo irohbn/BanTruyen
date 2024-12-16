6f32dd2a620f79d85e9efff830b0ebf0d80cd2d4
VanDung <trvdung2003@gmail.com>
2024-12-16 20:55:20 +0700

    testcomit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Import JS (bao gồm Popp
 import Header from "./page/Header";
 import Footer from "./page/Footer";
 import Body from "./page/Body";
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./CSS/Header.css";
 import "./CSS/Body.css";
 import "./App.css";
 import "./CSS/Shop.css";
+import Bookshop from "./page/Book/Bookshop";
+import "./../AUD/src2/cs/csss.css";
 function App() {
   return (
     <Router>
@@ -16,6 +18,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Body />} />
+          <Route path="/shop" element={<Bookshop />} />
         </Routes>
         <Footer />
       </div>
