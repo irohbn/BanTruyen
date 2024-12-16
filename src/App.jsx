7db4d0db5a6bcb3c3d8d3962f2969fba1218f4fb
VanDung <trvdung2003@gmail.com>
2024-12-16 21:47:48 +0700

    themvaocuakien

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter , Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import CSS
 import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Import JS (bao gồm Popper)
 import ProductDetail from "./productdetail/ProductDetail";
@@ -7,24 +7,21 @@ import Header from "./page/Header";
 import Footer from "./page/Footer";
 import Body from "./page/Body";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./CSS/Header.css";
 import "./CSS/Body.css";
 import "./App.css";
 import "./CSS/Shop.css";
-import Bookshop from "./page/Book/Bookshop";
-import "./../AUD/src2/cs/csss.css";
 function App() {
   return (
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Body />} />
-          <Route path="/product/:id" element={<ProductDetail />} />
-          <Route path="/search" element={<Search/>}/>
-        </Routes>
-        <Footer />
-      </div>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Body />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
